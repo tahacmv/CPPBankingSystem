708eb68a8466b7d8985f249b1bcc98fa9d1df4aa
tahacmv <tmmotassimahmedtaha@gmail.com>
2024-02-16 20:05:47 +0100

    initial commi

---
+++ test/test.cpp
@@ -0,0 +1,5 @@
+//
+// Created by tmmot on 16/02/2024.
+//
+
+#include "test.h"
---
+++ test/test.h
@@ -0,0 +1,14 @@
+//
+// Created by tmmot on 16/02/2024.
+//
+
+#ifndef CPPBANKINGSYSTEM_TEST_H
+#define CPPBANKINGSYSTEM_TEST_H
+
+
+class test {
+
+};
+
+
+#endif //CPPBANKINGSYSTEM_TEST_H
